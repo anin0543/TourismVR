ead917d5d4775a8d569e9710de1610f0381dbfb9
Deepak Gautam <anin0543@edu.easj.dk>
2021-02-25 23:26:47 +0100

    abcd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   AppRegistry,
   asset,
@@ -7,38 +7,40 @@ import {
   StyleSheet,
   Text,
   View,
-  VrButton
-} from 'react-360';
+  VrButton,
+} from "react-360";
 
-import {Surface} from 'react-360-web';
+import { Surface } from "react-360-web";
 const surfaceModule = NativeModules.surfaceModule;
 
 class InfoPanel extends React.Component {
   state = {
     img: {
-      name: 'info.png',
+      name: "info.png",
       width: 100,
-      height: 100
-    }
-  }
+      height: 101,
+    },
+  };
 
   transformDisplay(id) {
     this._changeSurfaceDimensions(500, 400, id);
-    this.setState({img: {
-      name: `${id}.jpg`,
+    this.setState({
+      img: {
+        name: `${id}.jpg`,
         width: 500,
-        height: 300
-      }
+        height: 300,
+      },
     });
   }
 
   resetPanel(id) {
     this._changeSurfaceDimensions(100, 100, id);
-    this.setState({img: {
-      name: 'info.png',
+    this.setState({
+      img: {
+        name: "info.png",
         width: 100,
-        height: 100
-      }
+        height: 100,
+      },
     });
   }
 
@@ -50,27 +52,32 @@ class InfoPanel extends React.Component {
     let { img } = this.state;
 
     return (
-      <View style={styles.displayPanel}
-            hitSlop={20}
-            onEnter={() => this.transformDisplay(this.props.id)}
-            onExit={() => this.resetPanel(this.props.id)}>
-        <Image source={asset(`${img.name}`)} style={{width: img.width, height: img.height}} />
+      <View
+        style={styles.displayPanel}
+        hitSlop={20}
+        onEnter={() => this.transformDisplay(this.props.id)}
+        onExit={() => this.resetPanel(this.props.id)}
+      >
+        <Image
+          source={asset(`${img.name}`)}
+          style={{ width: img.width, height: img.height }}
+        />
         <View style={styles.attractionBox}>
-          <Text style={styles.attractionText}>
-            {this.props.text}
-          </Text>
+          <Text style={styles.attractionText}>{this.props.text}</Text>
         </View>
       </View>
     );
   }
-};
-
+}
 
 export default class TourismVR extends React.Component {
   render() {
     return (
       <View>
-        <Image source={asset('poland.png')} style={{width: 500, height: 300}} />
+        <Image
+          source={asset("poland.png")}
+          style={{ width: 500, height: 300 }}
+        />
         <View style={styles.attractionBox}>
           <VrButton onClick={() => surfaceModule.start()}>
             <Text style={styles.attractionText}>
@@ -81,26 +88,26 @@ export default class TourismVR extends React.Component {
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   displayPanel: {
     width: 100,
     height: 100,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   attractionBox: {
     padding: 20,
-    backgroundColor: '#F7F7F7',
-    borderColor: '#C4002F',
+    backgroundColor: "#F7F7F7",
+    borderColor: "#C4002F",
     borderWidth: 2,
-    width: 500
+    width: 500,
   },
   attractionText: {
     fontSize: 30,
-    color: '#C4002F'
+    color: "#C4002F",
   },
 });
 
-AppRegistry.registerComponent('TourismVR', () => TourismVR);
-AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
+AppRegistry.registerComponent("TourismVR", () => TourismVR);
+AppRegistry.registerComponent("InfoPanel", () => InfoPanel);
